df526fbc17f4256c6e8ac103aae8ef8f5ab652ed
Dmitry Kolinchuk <dmko@sitecore.net>
2016-10-26 16:33:43 +0300

    Prevents from trimming a value in the Multi-Line Text field when saving it in Experience Editor

---
+++ src/Sitecore.Support.101295/CallServerSavePipeline.cs
@@ -0,0 +1,33 @@
+﻿using Sitecore.Caching;
+using Sitecore.Data;
+using Sitecore.ExperienceEditor.Speak.Server.Contexts;
+using Sitecore.ExperienceEditor.Speak.Server.Requests;
+using Sitecore.ExperienceEditor.Speak.Server.Responses;
+using Sitecore.ExperienceEditor.Switchers;
+using Sitecore.Globalization;
+using Sitecore.Pipelines;
+using Sitecore.Pipelines.Save;
+using System;
+
+namespace Sitecore.Support.ExperienceEditor.Speak.Ribbon.Requests.SaveItem
+{
+    public class CallServerSavePipeline : PipelineProcessorRequest<Sitecore.Support.ExperienceEditor.Speak.Server.Contexts.PageContext>
+    {
+        public override PipelineProcessorResponseValue ProcessRequest()
+        {
+            PipelineProcessorResponseValue pipelineProcessorResponseValue = new PipelineProcessorResponseValue();
+            Sitecore.Pipelines.Pipeline pipeline = Sitecore.Pipelines.PipelineFactory.GetPipeline("saveUI");
+            pipeline.ID = Sitecore.Data.ShortID.Encode(Sitecore.Data.ID.NewID);
+            Sitecore.Pipelines.Save.SaveArgs saveArgs = base.RequestContext.GetSaveArgs();
+            PipelineProcessorResponseValue result;
+            using (new ClientDatabaseSwitcher(base.RequestContext.Item.Database))
+            {
+                pipeline.Start(saveArgs);
+                Sitecore.Caching.CacheManager.GetItemCache(base.RequestContext.Item.Database).Clear();
+                pipelineProcessorResponseValue.AbortMessage = Sitecore.Globalization.Translate.Text(saveArgs.Error);
+                result = pipelineProcessorResponseValue;
+            }
+            return result;
+        }
+    }
+}
---
+++ src/Sitecore.Support.101295/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.101295")]
+[assembly: AssemblyProduct("Sitecore.Support.101295")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
